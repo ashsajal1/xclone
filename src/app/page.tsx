8db5680ad5037946d86ea900f1a28f60d112f041
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-02-28 23:59:53 +0800

    decrease the height of border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           {items.map((item) => (
             <div onClick={() => handleChange(item)} key={item} className='text-gray-400 hover:text-gray-200 cursor-pointer select-none text-center flex flex-col gap-2 items-center justify-between text-sm h-full hover:bg-gray-800 px-2'>
               <span className={`mt-4 ${selectedItem === item? 'font-bold text-slate-50':''}`}>{item}</span>
-              <div className={`h-[5px] w-full bg-blue-600 rounded-full ${selectedItem === item ? '' : 'hidden'}`}></div>
+              <div className={`h-[4px] w-full bg-blue-600 rounded-full ${selectedItem === item ? '' : 'hidden'}`}></div>
             </div>
           ))}
         </div>
