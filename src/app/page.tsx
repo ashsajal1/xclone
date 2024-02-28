3528b11bb52dcfd753c197e5983c0d366763455a
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-02-28 23:46:32 +0800

    Add border for selected items in header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+'use client';
+import { useState } from "react"
 
 export default function Home() {
+  const [selectedItem, setSelectedItem] = useState<null | string>(null)
+
+  const items = ['For you', 'Trending', 'Sports', 'Programming', 'Politics'];
+
+  const handleChange = (item: string) => {
+    setSelectedItem(item)
+  }
   return (
     <>
-    <nav>
-      <div className='h-[50px] border-b border-slate-500 flex items-center'>
-        <div className='text-gray-400 hover:text-gray-200 cursor-pointer select-none text-center flex items-center justify-center p-2 text-sm h-full hover:bg-gray-500'>For you</div>
-      </div>
-    </nav>
+      <nav>
+        <div className='h-[50px] border-b border-slate-500 flex items-center gap-2'>
+          {items.map((item) => (
+            <div onClick={() => handleChange(item)} key={item} className='text-gray-400 hover:text-gray-200 cursor-pointer select-none text-center flex flex-col gap-2 items-center justify-between text-sm h-full hover:bg-gray-800'>
+              <span className='mt-4'>{item}</span>
+              <div className={`h-[5px] w-full bg-blue-600 rounded-full ${selectedItem === item ? '' : 'hidden'}`}></div>
+            </div>
+          ))}
+        </div>
+      </nav>
     </>
   )
 }
