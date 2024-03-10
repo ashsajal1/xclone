0ea11f0efd322f1447593105605e249d6938ee57
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-10 19:18:41 +0800

    add dark and light mode design in top header option

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,8 +18,8 @@ export default function Home() {
         <nav className="w-full">
           <div className='h-[50px] border-b border-slate-500 flex items-center gap-2'>
             {items.map((item) => (
-              <div onClick={() => handleChange(item)} key={item} className='text-gray-400 hover:text-gray-200 cursor-pointer select-none text-center flex flex-col gap-2 items-center justify-between text-sm h-full hover:bg-gray-800 px-2'>
-                <span className={`mt-4 ${selectedItem === item ? 'font-bold text-slate-50' : ''}`}>{item}</span>
+              <div onClick={() => handleChange(item)} key={item} className='text-gray-800 dark:text-slate-50 hover:text-gray-900 dark:hover:text-slate-50 hover:font-bold cursor-pointer select-none text-center flex flex-col gap-2 items-center justify-between text-sm h-full hover:bg-gray-200 dark:hover:bg-gray-600 px-2'>
+                <span className={`mt-4 ${selectedItem === item ? 'font-bold text-gray-900 dark:text-slate-50' : ''}`}>{item}</span>
                 <div className={`h-[4px] w-full bg-blue-600 rounded-full ${selectedItem === item ? '' : 'hidden'}`}></div>
               </div>
             ))}
