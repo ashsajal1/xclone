95f061fa4aed9498ef1693968f48ade423ad581a
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-10 19:06:42 +0800

    Make left sidebar responsive

--- src/components/left-sidebar.tsx
+++ src/components/left-sidebar.tsx
@@ -11,25 +11,22 @@ export default function LeftSideBar() {
   }
 
   return (
-    <div className='md:w-1/3 border-r h-screen border-slate-500'>
+    <div className='hidden sm:block md:w-1/3 border-r h-screen border-slate-500'>
       <div className='md:w-full items-center flex justify-center p-4 text-blue-600 font-bold text-xl select-none'>
         xClone
       </div>
       <div>
         {navItems.map(nav => (
           <div className="mx-4" key={nav.name}>
-            <div onClick={() => handleSelectItem(nav.name)} className='p-2 cursor-pointer flex items-center justify-start w-full hover:bg-slate-500 rounded gap-1'>
+            <div onClick={() => handleSelectItem(nav.name)} className={`p-2 cursor-pointer flex items-center w-full hover:bg-slate-500 rounded gap-1 justify-center md:justify-start`}>
               {selectedItem === nav.name ? (
                 <>{nav.solidIcon}</>
               ) : (
                 <>{nav.outlineIcon}</>
               )}
-              <span>{nav.name}</span>
+              <span className="hidden md:block">{nav.name}</span>
             </div>
-
-
           </div>
-
         ))}
 
         <ModeToggle />
