5ba94f7dea00ebecc563b5722b202220fcd88d7a
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-13 00:23:28 +0800

    Add switch button to toggle mode

--- src/components/switch-button.tsx
+++ src/components/switch-button.tsx
@@ -1,20 +1,19 @@
+'use client'
+
 import React, { useState } from 'react';
 import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
+import { useTheme } from "next-themes"
 
 const SwitchButton = () => {
-    const [isOn, setIsOn] = useState(false);
-
-    const toggleSwitch = () => {
-        setIsOn(!isOn);
-    }
+    const { setTheme, theme } = useTheme();
 
     return (
-        <button title='switch mode' onClick={toggleSwitch} className='rounded border flex items-center'>
-            <div className={`p-2 rounded ${isOn ? '' : 'bg-yellow-600'}`}>
-                <MoonIcon className={`h-5 w-5 ${isOn? 'text-gray-600':'text-slate-50'}`} />
+        <button title='switch mode' className='rounded border dark:border-gray-600 flex items-center'>
+            <div onClick={() => setTheme("dark")} className={`p-2 rounded dark:bg-yellow-300`}>
+                <MoonIcon className={`h-5 w-5  dark:text-slate-50`} />
             </div>
-            <div className={`p-2 rounded ${isOn ? 'bg-yellow-600' : ''}`}>
-                <SunIcon className={`h-5 w-5 ${isOn? 'text-slate-50':'text-gray-600'}`} />
+            <div onClick={() => setTheme("light")} className={`p-2 rounded bg-yellow-300 text-slate-900 dark:text-slate-50 dark:bg-gray-900`}>
+                <SunIcon className={`h-5 w-5`} />
             </div>
         </button>
     );
